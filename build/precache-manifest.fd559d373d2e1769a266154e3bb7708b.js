a8939499f57225f68326009908c7fe28681fd551
Oskar Mroz <47431553+MoreeZ@users.noreply.github.com>
2020-08-26 12:53:02 +0100

    Network error fixed

---
+++ build/precache-manifest.fd559d373d2e1769a266154e3bb7708b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3058285cfba9ca0d72d4bf47dfdea54",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f626deee9c152a6fb15",
+    "url": "/static/css/main.28550761.chunk.css"
+  },
+  {
+    "revision": "721ef3155ae0b5d9d785",
+    "url": "/static/js/2.fe762cb0.chunk.js"
+  },
+  {
+    "revision": "9d251bcc2a2593b65ef17ce02ae1dca0",
+    "url": "/static/js/2.fe762cb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f626deee9c152a6fb15",
+    "url": "/static/js/main.6bca33dc.chunk.js"
+  },
+  {
+    "revision": "1e50bf51cea56fa22b0b",
+    "url": "/static/js/runtime-main.fc433cf7.js"
+  }
+]);
